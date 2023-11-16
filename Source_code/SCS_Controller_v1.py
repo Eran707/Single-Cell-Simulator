f45b62c5d6d2dada7bcb1d1b2c41f89b99892104
efs-jhu <eshorer1@jhu.edu>
2023-11-16 10:29:55 -0500

    reversion to the old simulation setup with no dynamic bicarb but including the NaH exchanger. Experiments set to run for 1200s but might need to be longer (to 12000, with all the other settings starting at a factor of 10 later). Simulations are setup with preset controllers for Joe and Kira.

--- Source_code/SCS_Controller_v1.py
+++ Source_code/SCS_Controller_v1.py
@@ -19,30 +19,52 @@ from SCS_Simulator_v1 import Simulator
 
 # STEP 1: ESTABLISHING SIMULATION TYPE
 
-sim_type = "Extend"    # Simulation type either set as "New" or "Extend"
+sim_type = "New"  # Simulation type either set as "New" or "Extend"
 
-new_file_name = "zChange_synapse_quarter_NAH_1e6dt"
+## ERAN
+new_file_name = "default_oldVersion"
 old_file_name = "rate_constants_SS_default_z_ratio_constants_exch"  # Only needed when the sim_type is Extend
+
+## JOE:
+
+# new_file_name = "z_change_oldVersion"
+
+## KIRA:
+
+# new_file_name = "KCC2_block_oldVersion"
 
 # STEP 2: SET SIMULATION TIMING
 
-total_t = 15    # total simulation time in seconds
-dt = 1e-6           # simulation time step in seconds
-intervals = 100000    # number of times the results of the simulation will be saved to the HDF file
+total_t = 1200  # total simulation time in seconds
+dt = 1e-6  # simulation time step in seconds
+intervals = 120000  # number of times the results of the simulation will be saved to the HDF file
 
 # STEP 3: SET SIMULATION SETTINGS
 
 infinite_bath = True
 dynamic_ATPase = True
 
-
 # STEP 4: SET ION CONCENTRATIONS (ONLY FOR NEW SIMULATIONS)
 
-intracellular_params = {"name": "1_Intracellular", "radius": 5e-5,"length": 25e-5,
-                        "na": 14e-3, "k":122.9e-3 , "cl":5.2e-3, "hco3":15e-3, "X":154.9e-3,"z":-0.85 }
 
+na_i = 13e-3
+k_i = 121.15e-3
+cl_i = 5.2e-3
+hco3_i = 10e-3
+# h_i = 6.31e-8 # we did not have this in the older version
+X_i = 139.9e-3
+Total_intra_charge = na_i + k_i - (cl_i + hco3_i + X_i * 0.85)
+intracellular_params = {"name": "1_Intracellular", "radius": 5e-5, "length": 25e-5,
+                        "na": na_i, "k": k_i, "cl": cl_i, "hco3": hco3_i, "X": X_i, "z": -0.85}
+na_o = 145e-3
+k_o = 3.5e-3
+cl_o = 110e-3
+hco3_o = 31e-3
+# h_o = 3.98e-8 - h_o
+X_o = 7.5e-3
+Total_extra_charge = na_o + k_o - (cl_o + hco3_o + X_o * 1)
 extracellular_params = {"name": "2_Extracellular", "radius": 5e-5, "length": 25e-5,
-                        "na": 145e-3,"k":3.5e-3 , "cl":110e-3, "hco3":31e-3, "X":7.5e-3,  "z":-1}
+                        "na": na_o, "k": k_o, "cl": cl_o, "hco3": hco3_o, "X": 7.5e-3, "z": -1}
 
 # STEP 5: ESTABLISH BASIC SIMULATION PARAMETERS
 
@@ -60,16 +82,28 @@ else:
 sim.set_timing(total_t=total_t, dt=dt, total_intervals=intervals)
 sim.infinite_bath = infinite_bath
 sim.dynamic_ATPase = dynamic_ATPase
-
-#sim.set_kcc2_off()
+# sim.set_kcc2_off()
 
 
 # STEP 6: ADD ADDITIONAL COMPONENTS TO THE SIMULATION
-# Options available: Z_change; X_change; Add_synapse; Add_current; Change_KCC2
 
-sim.add_synapse(start_t=10, tau=50e-3, max_g=1e-8)
+## Options available: Z_change; X_change; Add_synapse; Add_current; Change_KCC2
 
-#sim.set_z_change(start_t=3000,end_t=6000,z_change_amount=-0.2,adjust_cl=False)
-#sim.set_KCC2_change(start_t=3000, end_t=6000, final_KCC2_value=0)
+
+## ERAN
+
+sim.add_synapse(start_t=300, tau=50e-3, max_g=1e-8)
+
+## JOE
+
+# sim.add_synapse(start_t=300, tau=50e-3, max_g=1e-8)
+# sim.set_z_change(start_t=600,end_t=720,z_change_amount=-0.2,adjust_cl=False)
+# sim.add_synapse(start_t=900, tau=50e-3, max_g=1e-8)
+
+## KIRA
+
+# sim.add_synapse(start_t=300, tau=50e-3, max_g=1e-8)
+# sim.set_KCC2_change(start_t=600, end_t=720, final_KCC2_value=0)
+# sim.add_synapse(start_t=900, tau=50e-3, max_g=1e-8)
 
 sim.run_simulation()
